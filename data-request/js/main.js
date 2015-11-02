2d0a06ef94cb2aea2837097f9f2ced266542e323
Royce Le <royle@uw.edu>
2015-11-02 14:18:34 -0800

    lab

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,9 @@
 // Create application
-
+var myApp = angular.module('myApp', [])
 
 // Bind controller customersCtrl
+myApp.controller('customersCtrl', function($scope, $http){
+    $http.get('http://www.w3schools.com/angular/customers.php').then(function(response)){
+        $scope.data = response.data.records; 
+    }
+})
--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,10 @@
 // Create application
-
+var myApp = angular.module('myApp', [])
 
 // Bind controller madCtrl
-
+myApp.controller('madCtrl', function($scope){
+    $scope.name = "Andrew"; //placeholder
+})
 
 	// Set initial values for name, age, location, and food
 
